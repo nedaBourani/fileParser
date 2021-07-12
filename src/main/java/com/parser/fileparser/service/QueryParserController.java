340fbfc375e75a62061bcb668fb7fc7becd911e2
nedaBourani <87070886+nedaBourani@users.noreply.github.com>
2021-07-12 10:53:14 +0430

    add parser

--- src/main/java/com/parser/fileparser/service/QueryParserController.java
+++ src/main/java/com/parser/fileparser/service/QueryParserController.java
@@ -1,7 +1,7 @@
 package com.parser.fileparser.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.parser.fileparser.help.TaskRunner;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/parser")
 public class QueryParserController {
 
-    @Autowired
     QueryParserService queryParserService;
+    private TaskRunner taskRunner;
 
     @RequestMapping(value = "/get", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getStore(HttpServletRequest request) {
-
+        taskRunner.start();
         queryParserService.parse();
         return null;
     }
--- src/main/java/com/parser/fileparser/service/QueryParserService.java
+++ src/main/java/com/parser/fileparser/service/QueryParserService.java
@@ -1,5 +1,6 @@
 package com.parser.fileparser.service;
 
+import lombok.Synchronized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -7,40 +8,33 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class QueryParserService {
 
-    private final List<String> queryList;
-    public final Integer listSize = 10;
+    public List<String> queryList;
+    private Long indexOfQueryList = 0L;
 
-    @Value("${thread.size}")
-    Integer threadSize;
+    @Value("${query.file.path}")
+    String filePath;
 
     @Autowired
     public QueryParserService(@Value("${query.file.path}") String filePath) throws IOException {
+        readFile();
+    }
+
+    private void readFile() throws IOException {
         File file = new File(filePath);
         queryList = Files.readAllLines(file.toPath());
     }
 
+    @Synchronized
     public void parse() {
-        BlockingQueue<Integer> responseTimeQueue = new LinkedBlockingQueue<>(listSize);
+        getQuery();
+    }
 
-
-        //1-  thread run
-        // calc waite time
-        // call web service
-        // calc response time
-        // add to queue
-        //
-
-        // add response time
-        responseTimeQueue.add(10);
-
-        for (String s : queryList) {
-
-        }
+    private synchronized String getQuery() {
+        indexOfQueryList++;
+        return queryList.get(indexOfQueryList.intValue());
     }
 
 }
